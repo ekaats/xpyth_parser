7456ec6efc51862423cbaf68d3c116cd8082f616
Erwin Kaats <egit@tuta.io>
2021-06-13 22:52:45 +0200

    Functions without namespace are now automatically assumed to be part of the default function namespace.

--- conversion/function.py
+++ conversion/function.py
@@ -6,6 +6,10 @@ def get_function(v):
     qname = v[0]
     args = tuple(v[1:])
 
+    # If no prefix is defined, FN will be assumed for function calls
+    if qname.prefix is None:
+        qname.prefix = "fn"
+
     # Function name is an EQName. This name corresponds with a (buildin) function.
     # If no function is known, create a generic Function() object.
     # if qname in ["fn:count", "fn:avg", "fn:max", "fn:min", "fn:sum"]:
--- test/test_parsing_functions.py
+++ test/test_parsing_functions.py
@@ -21,28 +21,24 @@ class FunctionsOperatorsSequences(unittest.TestCase):
 
         # This returns a Python function which can be used elsewhere
         # The function contains the QName which identifies the function, as well as the arguments
-        self.assertEqual(count, Count(qname=QName(localname="count"), arguments=(1,2,3)))
+        self.assertEqual(count, Count(qname=QName(prefix="fn", localname="count"), arguments=(1,2,3)))
 
         # Functions can have a run() function which returns a value as described in the Xpath functions document
         #  (e.g., https://www.w3.org/TR/xpath-functions/#func-count)
         self.assertEqual(count.run(), 3)
 
         avg = t_XPath.parseString("avg(1,4,2,3,12,3,6)", parseAll=True)[0]
-        self.assertEqual(avg, Avg(qname=QName(localname="avg"), arguments=(1,4,2,3,12,3,6)))
+        self.assertEqual(avg, Avg(qname=QName(prefix="fn", localname="avg"), arguments=(1,4,2,3,12,3,6)))
         self.assertEqual(avg.run(), 4.428571428571429)
 
         max = t_XPath.parseString("max(1,4,2,3,12,3,6)", parseAll=True)[0]
-        self.assertEqual(max, Max(qname=QName(localname="max"), arguments=(1,4,2,3,12,3,6)))
+        self.assertEqual(max, Max(qname=QName(prefix="fn", localname="max"), arguments=(1,4,2,3,12,3,6)))
         self.assertEqual(max.run(), 12)
 
         min = t_XPath.parseString("min(4,2,3,12,3,6)", parseAll=True)[0]
-        self.assertEqual(min, Min(qname=QName(localname="min"), arguments=(4,2,3,12,3,6)))
+        self.assertEqual(min, Min(qname=QName(prefix="fn", localname="min"), arguments=(4,2,3,12,3,6)))
         self.assertEqual(min.run(), 2)
 
         sum = t_XPath.parseString("sum(1,4,2,3,12,3,6)", parseAll=True)[0]
-        self.assertEqual(sum, Sum(qname=QName(localname="sum"), arguments=(1,4,2,3,12,3,6)))
+        self.assertEqual(sum, Sum(qname=QName(prefix="fn", localname="sum"), arguments=(1,4,2,3,12,3,6)))
         self.assertEqual(sum.run(), 31)
-
-
-
-
