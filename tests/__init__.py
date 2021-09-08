90fa22a5a2c4a44478b7a18907c379d4438a97e3
Erwin Kaats <egit@tuta.io>
2021-09-08 20:22:38 +0200

    Trying to get tests to work remotely

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,4 +1,2 @@
-import os
 import sys
-sys.path.insert(0, os.path.abspath( os.path.join(os.path.dirname(__file__), 
-                                               '../src/') ))
+sys.path.append("/home/runner/work/xpyth_parser/xpyth_parser/src")
